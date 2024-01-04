0d7c2733af488461abec9e5953bf36ffc4214b64
aswin <aswin0484@gmail.com>
2024-01-04 16:55:30 +0530

    removed files

--- contats/lib/src/core/colors/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color whites = Colors.white;
-  static const Color titleBackground = Color.fromARGB(255, 6, 154, 151);
-}
--- contats/lib/src/icons/icons.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class IconsWidget {
-  static const Icon person = Icon(Icons.person_4_outlined);
-  static const Icon add = Icon(Icons.add);
-}
